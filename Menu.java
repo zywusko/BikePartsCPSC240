da45f4aecb1e17df1e1aba687fbfadf305b46ba8
abarbuzza <55552377+abarbuzza@users.noreply.github.com>
2019-09-26 13:34:55 -0400

    Added an updated menu file

--- Menu.java
+++ Menu.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class Menu {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
+		String filename = null;
+	    Scanner filescnr = new Scanner(new File(filename));
 		Scanner scnr = new Scanner(System.in);
-		System.out.println("Please select your option from the following menu:");
+		System.out.println("Please select your option from the following menu:"); //lists all options for the user to enter
 		System.out.println("Read: Read a delivery file");
 		System.out.println("Enter: Enter a part");
 		System.out.println("Sell: Sell a part");
@@ -17,47 +19,63 @@ public class Menu {
 		System.out.println("Enter your choice:");
 		String answer = scnr.next();
 		int i = 0;
+		
 		while (i == 0) {
-			if (answer == "Read") {
-
+			if (answer.equalsIgnoreCase("Read")) {
 				i = 1;
-
+				System.out.println("What is the name of the inventory delivery file?");
+				filename = scnr.next();
+				try{
+					filescnr.hasNext();
+				}
+				catch(Exception e){
+					System.out.println("File does not exist.");
+				}
 			}
-			if (answer == "Enter") {
-
+			else if (answer.equalsIgnoreCase("Enter")) {
 				i = 1;
-
+				System.out.println("What is the part name?");
+				String partName = scnr.next();
+				System.out.println("What is the part number?");
+				long partNum = scnr.nextLong();
+				System.out.println("What is the list price?");
+				double listPrice = scnr.nextDouble();
+				System.out.println("What is the sales price?");
+				double salesPrice = scnr.nextDouble();
+				System.out.println("Is the part on sale?  True or false?");
+				boolean onSale = scnr.nextBoolean();
+				System.out.println("What is the quantity of this part in stock?");
+				int partQuantity = scnr.nextInt();
+				
 			}
-			if (answer == "Sell") {
-
+			else if (answer.equalsIgnoreCase("Sell")) {
 				i = 1;
-
+				System.out.println("What is the part number?");
+				long partNum = scnr.nextLong();
+				
 			}
-			if (answer == "Display") {
-
+			else if (answer.equalsIgnoreCase("Display")) {
 				i = 1;
-
+				System.out.println("What is the part name?");
+				String partName = scnr.next();
 			}
-			if (answer == "SortName") {
-
+			else if (answer.equalsIgnoreCase("SortName")) {
 				i = 1;
-
+				
 			}
-			if (answer == "SortNumber") {
-
+			else if (answer.equalsIgnoreCase("SortNumber")) {
 				i = 1;
-
+				
 			}
-			if (answer == "Quit") {
-
+			else if (answer.equalsIgnoreCase("Quit")) {
 				i = 1;
-
+				
 			}
-			else {
-				System.out.println("Invalid selection.  Input is case sensitive.");
+			else{
+				System.out.println("Invalid selection");
 				answer = scnr.next();
-				i = 0;
 			}
 		}
+		scnr.close();
 	}
 }
